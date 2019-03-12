f5ca46814690f3751a02a4abb36b9ec58d373164
Christopher Hiller <boneskull@boneskull.com>
2019-03-12 11:57:53 -0700

    fix timeout/slow string values and duplicate arguments

--- test/node-unit/cli/options.spec.js
+++ test/node-unit/cli/options.spec.js
@@ -626,7 +626,7 @@ describe('options', function() {
           findupSync
         });
 
-        expect(loadOptions(), 'to satisfy', {timeout: 800, require: ['foo']});
+        expect(loadOptions(), 'to satisfy', {timeout: '800', require: ['foo']});
       });
 
       it('should prioritize package.json over mocha.opts', function() {
@@ -689,7 +689,7 @@ describe('options', function() {
           loadOptions('--timeout 500'),
           'to have property',
           'timeout',
-          500
+          '500'
         );
       });
     });
